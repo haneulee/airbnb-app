32eb8248256a0087b27f33077b79bdde77341ca2
Haneul Lee <lovesky4294@gmail.com>
2020-01-17 17:57:33 +0900

    update, delete room

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -32,10 +32,36 @@ class WriteRoomSerializer(serializers.Serializer):
         return Room.objects.create(**validated_data)
 
     def validate(self, data):
-        check_in = data.get("check_in")
-        check_out = data.get("check_out")
+        # 처음 room을 생성할 때만 체크하도록 변경, instance가 있으면 이미 생성된 room
+        # update (self.instance.check_in > default 값)
+        if self.instance:
+            check_in = data.get("check_in", self.instance.check_in)
+            check_out = data.get("check_out", self.instance.check_out)
+        else:
+            # create
+            check_in = data.get("check_in")
+            check_out = data.get("check_out")
         if check_in == check_out:
             raise serializers.ValidationError(
                 "Not enough time between changes")
-        else:
-            return data
+        return data
+
+    def update(self, instance, validated_data):
+        # 오른쪽 인자는 default 값
+        instance.name = validated_data.get("name", instance.name)
+        instance.address = validated_data.get("address", instance.address)
+        instance.price = validated_data.get("price", instance.price)
+        instance.beds = validated_data.get("beds", instance.beds)
+        instance.lat = validated_data.get("lat", instance.lat)
+        instance.lng = validated_data.get("lng", instance.lng)
+        instance.bedrooms = validated_data.get("bedrooms", instance.bedrooms)
+        instance.bathrooms = validated_data.get(
+            "bathrooms", instance.bathrooms)
+        instance.check_in = validated_data.get("check_in", instance.check_in)
+        instance.check_out = validated_data.get(
+            "check_out", instance.check_out)
+        instance.instant_book = validated_data.get(
+            "instant_book", instance.instant_book
+        )
+        instance.save()
+        return instance
